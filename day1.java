751cc4a7d0ceec45a10e618236c0d65fc5f5bca6
Shivani Katkar <shivanikatkar04@gmail.com>
2025-08-15 21:04:31 +0530

    Update day1.java
    sort_0_1

--- day1.java
+++ day1.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public void sort(int[] nums) {
+        int[] count=new int[3];
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]]=count[nums[i]]+1;
+        }
+        int k=0;
+        for(int i=0;i<count.length;i++){
+            for(int j=0;j<count[i];j++){
+                nums[k]=i;
+                k++;
+            }
+        }
+        
+    }
+  public static void main(String args[]){
+  int[] arr={1,0,2,0,1,1,2};
+  sort(arr);
+  for(int i=0;i<arr.length;i++){
+    System.out.println(arr[i]);
+  }
+  }
+}
